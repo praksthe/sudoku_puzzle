698041cc86bb0790e88fa530cd95e9ac87f9a0f2
prakash shukla <prakashu@cisco.com>
2019-12-09 00:19:25 +0530

    time taken feature and test case updated

--- static/js/sudoku.js
+++ static/js/sudoku.js
@@ -16,17 +16,16 @@ $(function () {
         //     game.drawBoard(JSON.parse(data));
         // });
         var json_data = JSON.stringify({ data: puzzle });
-        console.log(json_data);
+        var time_counter = $.now();
         $.ajax({
             contentType: 'application/json',
             data: json_data,
             dataType: 'json',
             success: function (data) {
-                console.log(data);
+                
                 var counter = 0;
                 for (i = 0; i < 9; i++) {
                     for (j = 0; j < 9; j++) {
-                        console.log('#solution-cell-' + counter);
                         $('#solution-cell-' + counter).val(data[i][j]);
                         counter++;
                     }
@@ -34,17 +33,20 @@ $(function () {
                 }
             },
             error: function (e) {
+                time_counter = $.now() - time_counter;
                 if(e.status == 400) {
                     alert('The sudoku puzzle inputs are invalid. \n Please try again');
                 } else {
                     console.log(e);
                     alert('There was an error in puzzle problem. \n'+ e.statusText);
                 }
-                
             },
             processData: false,
             type: 'POST',
             url: 'http://localhost:5000/sudoku_puzzle'
         });
+        time_counter = $.now() - time_counter;
+        $('#time-taken').text('Time taken: '+ time_counter)
+        console.log(time_counter);
     });
 });
--- test_sudo.py
+++ test_sudo.py
@@ -38,6 +38,7 @@ class BasicTests(unittest.TestCase, PuzzleSudoku):
         print "Final table scernario 1"
         self.pretty_print_in_2D(response.json())
         print "====end====="
+
     def test_sunny_day_scenario2(self):
         ###############
         #Positive scenario : input_json is taken and solved suduko puzzle.
@@ -52,7 +53,7 @@ class BasicTests(unittest.TestCase, PuzzleSudoku):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), expected_result)
-        print "\nFinal table scernario 2"
+        print "\nFinal table sunny scernario 2"
         self.pretty_print_in_2D(response.json())
         print "====end====="
 
@@ -64,20 +65,19 @@ class BasicTests(unittest.TestCase, PuzzleSudoku):
         ###############
 
         input_json = "4..12.6...75..9...6.1.78..7.4......1.5....9.4.6...5.7.3...1..2....4....9."
-        expected_result = [[7, 3, 5, 4, 8, 9, 1, 2, 6], [6, 1, 8, 2, 7, 5, 3, 4, 9], [4, 9, 2, 6, 3, 1, 5, 7, 8], [2, 5, 7, 8, 4, 3, 9, 6, 1], [3, 6, 1, 9, 5, 2, 7, 8, 4], [9, 8, 4, 1, 6, 7, 2, 3, 5], [5, 7, 6, 3, 9, 4, 8, 1, 2], [8, 2, 3, 5, 1, 6, 4, 9, 7], [1, 4, 9, 7, 2, 8, 6, 5, 3]]
 
         response = requests.post('http://localhost:5000/sudoku_puzzle', json=dict(data = input_json))
 
-        import pdb;pdb.set_trace()
+        #import pdb;pdb.set_trace()
         self.assertEqual(response.status_code, 400)
-        print("json is not of expected length of 81\n")
+        print("rainy-scenario1==> json is not of expected length of 81\n")
 
     def test_rainy_day_scenario2(self):
         ###############
         #Negative scenario : input_json is have invalid input for UI
         '''
-        |7 8 0|
-        |7 0 0|==========> should not have 7 again(this sample is taken from below input_json)
+        |7 8 0|7==========> should not have 7 again(this sample is taken from below input_json)
+        |0 0 0|
         |1 2 0|
         '''
         #Validation        : status code and response value is validated.
@@ -85,15 +85,31 @@ class BasicTests(unittest.TestCase, PuzzleSudoku):
         ###############
 
         input_json = "78.7..12.6...75..9...6.1.78..7.4.26...1.5.93.9.4.6...5.7.3...1212...74...492.6..7"
-        expected_result = [[7, 3, 5, 4, 8, 9, 1, 2, 6], [6, 1, 8, 2, 7, 5, 3, 4, 9], [4, 9, 2, 6, 3, 1, 5, 7, 8], [2, 5, 7, 8, 4, 3, 9, 6, 1], [3, 6, 1, 9, 5, 2, 7, 8, 4], [9, 8, 4, 1, 6, 7, 2, 3, 5], [5, 7, 6, 3, 9, 4, 8, 1, 2], [8, 2, 3, 5, 1, 6, 4, 9, 7], [1, 4, 9, 7, 2, 8, 6, 5, 3]]
 
         response = requests.post('http://localhost:5000/sudoku_puzzle', json=dict(data = input_json))
 
-        import pdb;pdb.set_trace()
         self.assertEqual(response.status_code, 400)
-        print("Invalid input val from user in table\n")
-        
+        print("rainy-scenario2 ==> Invalid input val 7 in the Table[0][4] of the table from user\n")
 
+    def test_rainy_day_scenario3(self):
+        ###############
+        #Negative scenario : input_json is have invalid input for UI
+        '''
+        |7 8 0|
+        |6 8 0|==========> should not have 8 again(this sample is taken from below input_json)
+        |1 2 0|
+        '''
+        #Validation        : status code and response value is validated.
+        #Final table       : Final resulatant table is not created (400) invalid input from user
+        ###############
+
+        input_json = "78....12.68..75..9...6.1.78..7.4.26...1.5.93.9.4.6...5.7.3...1212...74...492.6..7"
+
+        response = requests.post('http://localhost:5000/sudoku_puzzle', json=dict(data = input_json))
+
+        self.assertEqual(response.status_code, 400)
+        print("rainy-scenario3 ==> Invalid input val 8 in the Table[1][2] of the table from user\n")
+     
 
 
 if __name__ == "__main__":
